310ae6f13dee5524d0b759867f827747a8465b5d
Gustavo Delazei <gustavo.delazeri07@gmail.com>
2021-12-06 11:03:46 -0300

    add day 06

---
+++ d06.jl
@@ -0,0 +1,26 @@
+function parse_input(input)
+    return map((x) -> (parse(Int64, x)), split(chomp(readline(input)), ","))
+end
+
+function part_1_and_2(MAX_DAYS)
+    input = parse_input("data/d06.txt")
+    counter = zeros(Int128,(9,))
+
+    for i in input
+        counter[i] += 1
+    end
+
+    for day in 1:(MAX_DAYS - 1)
+        new_borns = counter[1]
+        for i in 1:8
+            counter[i] = counter[i+1]
+        end
+        counter[9] = new_borns
+        counter[7] += new_borns
+    end
+    
+    println(sum(counter))
+end
+
+part_1_and_2(80)
+part_1_and_2(256)
